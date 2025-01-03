7aefd968f8c696a481d09c416fdc2187cf728218
lailIzza <lailatulizzah@saintek.unipdu.ac.id>
2025-01-03 12:25:58 +0700

    perbaikan link

--- navbar.php
+++ navbar.php
@@ -89,14 +89,14 @@
     <div class="top-navbar">
         <!-- Navbar 1-->
         <div class="nav-links">
-            <a href="produk.php">Produk</a>
-            <a href="panduan.php">Panduan Belanja</a>
+            <a href="../produk.php">Produk</a>
+            <a href="../panduan.php">Panduan Belanja</a>
             <a href="#">Tentang Kami</a>
         </div>
     </div>
     <nav class="main-navbar">
         <!-- Navbar 2-->
-        <a href="home.php" class="logo">Bekas.<span style="color: #fcbf49;">Id</span></a>
+        <a href="../home.php" class="logo">Bekas.<span style="color: #fcbf49;">Id</span></a>
 
         <div class="search-bar">
             <input type="text" placeholder="Cari produk" id="searchInput">
